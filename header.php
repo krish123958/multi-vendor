2ed3359101fdd758efb5c62e4a6213f750fd9066
vamsi konakala <60782942+krish123958@users.noreply.github.com>
2021-07-16 12:00:51 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>HEADER</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/headerstyle.css">
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script>
+            $(document).ready(function(){
+                $('#icon').click(function(){
+                    $('ul').toggleClass('show');
+                });
+            });
+
+        </script>
+    </head>
+    <body>
+    <nav>
+        <label class="logo">JHONNY</label>
+        <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">services</a></li>
+            <li><a href="#">myaccount</a></li>
+
+
+        </ul>
+        <label id="icon">
+        <i class="fas fa-bars"></i>
+        
+        
+        
+        </label>
+        
+        
+        </nav> 
+        <section></section>
+    </body>
+</html>
